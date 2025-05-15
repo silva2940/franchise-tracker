8db2b8d60755654f698bad6a767142a1b65672f6
silva2940 <brendanasilva10@icloud.com>
2025-05-15 16:53:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import Dashboard from './components/Dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Franchise Tracker</h1>
-      <Dashboard />
-    </div>
-  );
-}
-
-export default App;
